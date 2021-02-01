1d59b3b409b23609b26fd65ee813b364133094e1
Matt Lemche <mattlemche@gmail.com>
2021-02-01 10:45:08 -0500

    Add color variables and header styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -11,9 +11,12 @@ export default function Header(props) {
                 Urban Cyclists
             </h1>
             <div className="title-box">
-            <h2 className="header__subtitle">
-             A tough-love letter to fellow cyclists in Toronto.
-             </h2>
+                <h2 className="header__subtitle">
+                A tough-love letter to fellow cyclists in Toronto.
+                </h2>
+                <svg className="polygon" height="150" width="100%">
+                    <polygon points="05,10 325,05 330,95 00,100" />
+                </svg>
             </div>            
         </header>
     );
--- client/src/components/Panel/Panel.js
+++ client/src/components/Panel/Panel.js
@@ -4,26 +4,29 @@ import data from '../../data';
 
 export default function Panel(props) {
     return (
-        <article className="panel">
+        <main className="main">
             {data.map(rule => {
                 return (
-                    <div className="panel__content">
-                    <h3 className="panel__title">
-                        {rule.title}
-                    </h3>
-                    <p className="panel__body">
-                        {rule.body}
-                    </p>
-                    <div className="panel__image">
-                        <img 
-                        src={rule.imageUrl} 
-                        alt={rule.alt} 
-                        className="image"/>
+                    <div className="panel">
+                        <div className="panel__content">
+                            <h3 className="panel__title">
+                                {rule.title}
+                            </h3>
+                            <p className="panel__body">
+                                {rule.body}
+                            </p>
+                            <div className="panel__image">
+                                <img 
+                                src={rule.imageUrl} 
+                                alt={rule.alt} 
+                                className="image"/>
+                            </div>
+                        </div>
                     </div>
-                    </div>
+                    
                     
                 )
             })}
-        </article>
+        </main>
     );
 };
